0ca13d875c7defa03c0170c12187f25076c161f6
lezvieprod <ilyxasokol2014@mail.ru>
2021-04-05 22:08:13 +0200

    weather app final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const weatherApi = {
+    key: "610a9f2255ef4935042e049162ca8c32",
+    urlbase: "https://api.openweathermap.org/data/2.5/"
+  }
+
+  const [weatherInfo, setWeatherInfo] = useState({})
+  const [city, setCity] = useState('');
+  console.log(weatherInfo);
+
+  const getWeather = async () => {
+    try {
+      let response = await fetch(`${weatherApi.urlbase}weather?q=${city}&lang=ru&units=metric&appid=${weatherApi.key}`);
+      let weather = await response.json();
+      weather && setWeatherInfo(weather);
+    }
+    catch {
+      alert('undefined error')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-layout">
+      <div className="app-body">
+        <input
+          value={city}
+          onChange={e => setCity(e.target.value)}
+        />
+        <button onClick={getWeather}>get weather</button>
+        <div className="app-output-info">
+          {(typeof weatherInfo.weather !== "undefined") &&
+            <div className="weather-output">
+              <div className="weather-output__city">
+                {weatherInfo.name + ', ' + weatherInfo.sys.country}
+              </div>
+              <div>
+                {weatherInfo.weather.map(i => i.description)}
+              </div>
+              <div>
+                {'Температура сейчас: ' + Math.round(weatherInfo.main.temp)}
+              </div>
+              <div>
+                {'Максимальная температура: ' + Math.round(weatherInfo.main.temp_max)}
+              </div>
+              <div>
+                {'Минимальная температура: ' + Math.round(weatherInfo.main.temp_min)}
+              </div>
+            </div>
+          }
+          {weatherInfo.message &&
+            <div style={{ flexWrap: 'wrap' }}>
+              <div>
+                {'Error code: ' + weatherInfo.cod}
+              </div>
+              <div>
+                {weatherInfo.message}
+              </div>
+            </div>
+          }
+        </div>
+      </div>
     </div>
   );
 }
